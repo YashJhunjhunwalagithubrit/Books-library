01635b1e0ddfdd6d20be7f8da4e53101e6fd93dc
Yash <yashjhunjhunwala700@gmail.com>
2024-09-01 14:57:21 +0530

    updated

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,14 +1,18 @@
 import react ,{useState}from "react";
+import Card from "./Card";
 import axios from "axios";
 const Main=()=>{
     const [search,setSearch]=useState("");
     const [bookData,setData]=useState([]);
+    const api_key = process.env.REACT_APP_API_KEY;
     const searchBook=(evt)=>{
-        if(evt.key==="AIzaSyDd9C_ZA1Ekii0xCehUjqReSLGAd5aKhLw")
+        if(evt.key==="Enter")
         {
-            axios.get('https://www.googleapis.com/books/v1/volumes?q='+search+'&key=AIzaSyA6SaT23KNiiA6DnUfUQTvFeyAcQEkwnSU'+'&maxResults=40')
-            .then(res=>setData(res.data.items))
-            .catch(err=>console.log(err))
+            axios.get('https://www.googleapis.com/books/v1/volumes?q='+search+`&key=${api_key}`+'&maxResults=40')
+            .then(res=>{
+                console.log("books", res.data);
+                setData(res.data.items)})
+            .catch(err=>console.log("error",err))
         }
     }
     return(
@@ -27,6 +31,11 @@ const Main=()=>{
                     </div>
                     <img src="./images/bg2.png" alt="" />
                 </div>
+            </div>
+            <div className="container">
+                {
+                    <Card book={bookData}/>
+                }
             </div>
 
            
